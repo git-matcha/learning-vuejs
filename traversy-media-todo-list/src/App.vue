ad35fe047f970842c3e3d8e5f8085438fb9823bd
Haley Sanders <haleyfsanders@gmail.com>
2020-02-04 19:38:01 -0500

    committing some old changes

---
+++ traversy-media-todo-list/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id:1, 
+          title: "ToDo one",
+          completed:false
+        },
+        {
+          id:2, 
+          title: "ToDo Two",
+          completed:true
+        },
+        {
+          id:3, 
+          title: "ToDo three",
+          completed:false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
